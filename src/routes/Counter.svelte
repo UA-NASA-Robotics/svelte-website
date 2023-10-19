3f9f4c6fd02cfa2a7ea1972e58f0f7d68d68f1a8
nobrien1 <omgnateobrien@gmail.com>
2023-10-19 14:52:08 -0400

    light and dark mode header

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -42,6 +42,11 @@
 		margin: 1rem 0;
 	}
 
+	:global(body.dark) .counter {
+		border-top: 1px solid rgba(255, 255, 255, 0.1);
+		border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+	}
+
 	.counter button {
 		width: 2em;
 		padding: 0;
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,82 +1,105 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+
+	const Themes = {
+		Light: 'Light',
+		Dark: 'Dark'
+	} as const;
+
+	type ThemeKeys = keyof typeof Themes;
+	type Theme = (typeof Themes)[ThemeKeys];
+
+	let theme: Theme = Themes.Light;
+
+	function changeTheme() {
+		theme = theme == Themes.Light ? Themes.Dark : Themes.Light;
+		if (theme === 'Dark') window.document.body.classList.add('dark');
+		else window.document.body.classList.remove('dark');
+	}
 </script>
 
 <header>
-	<div class="corner">
+	<!-- <div class="corner">
 		<a href="https://kit.svelte.dev">
 			<img src={logo} alt="SvelteKit" />
 		</a>
-	</div>
+	</div> -->
+
+	<img src="https://uanasarobotics.org/Images/Logo.png" />
 
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
+		<!-- <svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
+		</svg> -->
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+				<a href="/" aria-current={$page.url.pathname === '/' ? 'page' : undefined}>Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+				<a href="/about" aria-current={$page.url.pathname === '/about' ? 'page' : undefined}
+					>Competition</a
+				>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+				<a
+					href="/sverdle"
+					aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>History</a
+				>
+			</li>
+			<li>
+				<a>New Members</a>
+			</li>
+			<li>
+				<a>Sponsers</a>
+			</li>
+			<li>
+				<a>Outreach</a>
+			</li>
+			<li>
+				<a>Officers</a>
+			</li>
+			<li>
+				<a>Contact Us</a>
 			</li>
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
+		<!-- <svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
+		</svg> -->
 	</nav>
 
-	<div class="corner">
+	<div class="switch-container">
+		<p>{theme}</p>
+		<label class="switch">
+			<input type="checkbox" on:change={changeTheme} value={theme === Themes.Dark} />
+			<span class="slider round" />
+		</label>
+	</div>
+
+	<!-- <div class="corner">
 		<a href="https://github.com/sveltejs/kit">
 			<img src={github} alt="GitHub" />
 		</a>
-	</div>
+	</div> -->
 </header>
 
 <style>
 	header {
 		display: flex;
 		justify-content: space-between;
+		background-color: var(--light-bg-primary);
+		transition: color var(--transition-length) linear;
+		-webkit-transition: var(--transition-length);
 	}
 
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
+	:global(body.dark) header {
+		background-color: var(--dark-bg-primary);
 	}
 
 	nav {
 		display: flex;
 		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
+		flex: 1;
+		align-self: center;
 	}
 
 	ul {
@@ -88,7 +111,6 @@
 		justify-content: center;
 		align-items: center;
 		list-style: none;
-		background: var(--background);
 		background-size: contain;
 	}
 
@@ -105,8 +127,23 @@
 		position: absolute;
 		top: 0;
 		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
+		transition: color var(--transition-length) linear;
+		-webkit-transition: var(--transition-length);
+	}
+
+	p {
+		color: var(--light-txt-primary);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color var(--transition-length) linear;
+		-webkit-transition: var(--transition-length);
+	}
+
+	:global(body.dark) p {
+		color: var(--dark-txt-primary);
 	}
 
 	nav a {
@@ -114,16 +151,125 @@
 		height: 100%;
 		align-items: center;
 		padding: 0 0.5rem;
-		color: var(--color-text);
+		color: var(--light-txt-primary);
 		font-weight: 700;
 		font-size: 0.8rem;
 		text-transform: uppercase;
 		letter-spacing: 0.1em;
 		text-decoration: none;
-		transition: color 0.2s linear;
+		transition: color var(--transition-length) linear;
+		-webkit-transition: var(--transition-length);
+	}
+
+	:global(body.dark) nav a {
+		color: var(--dark-txt-primary);
 	}
 
 	a:hover {
-		color: var(--color-theme-1);
+		color: var(--light-accent);
+		transition: color var(--transition-length) linear;
+		-webkit-transition: var(--transition-length);
+	}
+
+	:global(body.dark) a:hover {
+		color: var(--dark-accent);
+	}
+
+	a[aria-current='page'] {
+		background-color: var(--light-bg-secondary);
+		transition: color var(--transition-length) linear;
+		-webkit-transition: var(--transition-length);
+	}
+
+	:global(body.dark) a[aria-current='page'] {
+		background-color: var(--dark-bg-secondary);
+	}
+
+	img {
+		height: 100px;
+		align-self: center;
+		margin: 5px;
+	}
+
+	.switch-container {
+		align-items: center;
+		margin: 20px;
+		align-self: center;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.switch {
+		position: relative;
+		display: inline-block;
+		width: 60px;
+		height: 34px;
+	}
+
+	/* Hide default HTML checkbox */
+	.switch input {
+		opacity: 0;
+		width: 0;
+		height: 0;
+	}
+
+	/* The slider */
+	.slider {
+		position: absolute;
+		cursor: pointer;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: var(--light-txt-secondary);
+		-webkit-transition: var(--transition-length);
+		transition: var(--transition-length);
+	}
+
+	.slider:before {
+		position: absolute;
+		content: '';
+		height: 26px;
+		width: 26px;
+		left: 4px;
+		bottom: 4px;
+		background-color: white;
+		-webkit-transition: var(--transition-length);
+		transition: var(--transition-length);
+	}
+
+	input:checked + .slider {
+		background-color: var(--light-accent);
+		transition: color var(--transition-length) linear;
+		-webkit-transition: var(--transition-length);
+	}
+
+	input:focus + .slider {
+		box-shadow: 0 0 1px var(--light-accent);
+		transition: color var(--transition-length) linear;
+		-webkit-transition: var(--transition-length);
+	}
+
+	:global(body.dark) input:checked + .slider {
+		background-color: var(--dark-accent);
+	}
+
+	:global(body.dark) input:focus + .slider {
+		box-shadow: 0 0 1px var(--dark-accent);
+	}
+
+	input:checked + .slider:before {
+		-webkit-transform: translateX(26px);
+		-ms-transform: translateX(26px);
+		transform: translateX(26px);
+	}
+
+	/* Rounded sliders */
+	.slider.round {
+		border-radius: 34px;
+	}
+
+	.slider.round:before {
+		border-radius: 50%;
 	}
 </style>
