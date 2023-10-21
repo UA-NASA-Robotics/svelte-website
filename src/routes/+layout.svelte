aceb9b243f3206c58ec85171023934329603cbc1
nobrien1 <omgnateobrien@gmail.com>
2023-10-21 15:55:31 -0400

    Add footer with social

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,24 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<div>
+			<p>Social</p>
+			<a class="facebook-button">
+				<img src="https://logodownload.org/wp-content/uploads/2014/09/facebook-logo-1-2.png" />
+			</a>
+			<a class="instagram-button">
+				<img src="https://clipartcraft.com/images/instagram-logo-transparent-png-format-3.png" />
+			</a>
+			<a class="twitter-button">
+				<img src="https://freelogopng.com/images/all_img/1690643591twitter-x-logo-png.png" />
+			</a>
+			<a class="youtube-button">
+				<img
+					src="https://hdclipartall.com/images/youtube-clipart-youtube-square-icons-1600x1600.png"
+				/>
+			</a>
+		</div>
+		<p>© University of Akron NASA RMC Team 2023-2024</p>
 	</footer>
 </div>
 
@@ -43,6 +60,40 @@
 
 	footer a {
 		font-weight: bold;
+	}
+
+	footer div {
+		display: flex;
+		column-gap: 10px;
+		align-items: center;
+	}
+
+	footer div a {
+		width: 50px;
+		height: 50px;
+		align-items: center;
+		justify-content: center;
+		display: flex;
+	}
+
+	.facebook-button {
+		background-color: blue;
+	}
+
+	.instagram-button {
+		background-color: blueviolet;
+	}
+
+	.twitter-button {
+		background-color: black;
+	}
+
+	.youtube-button {
+		background-color: red;
+	}
+
+	footer div a img {
+		width: 30px;
 	}
 
 	@media (min-width: 480px) {
