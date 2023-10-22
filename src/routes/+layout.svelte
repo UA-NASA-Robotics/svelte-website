44d02c17e13367a4d5ebc7e6abd7b8e2e8b41696
nobrien1 <omgnateobrien@gmail.com>
2023-10-22 00:01:00 -0400

    remove scss and make footer better

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
+	import Footer from '../components/Footer.svelte';
 </script>
 
 <div class="app">
@@ -10,26 +11,7 @@
 		<slot />
 	</main>
 
-	<footer>
-		<div>
-			<p>Social</p>
-			<a class="facebook-button">
-				<img src="https://logodownload.org/wp-content/uploads/2014/09/facebook-logo-1-2.png" />
-			</a>
-			<a class="instagram-button">
-				<img src="https://clipartcraft.com/images/instagram-logo-transparent-png-format-3.png" />
-			</a>
-			<a class="twitter-button">
-				<img src="https://freelogopng.com/images/all_img/1690643591twitter-x-logo-png.png" />
-			</a>
-			<a class="youtube-button">
-				<img
-					src="https://hdclipartall.com/images/youtube-clipart-youtube-square-icons-1600x1600.png"
-				/>
-			</a>
-		</div>
-		<p>© University of Akron NASA RMC Team 2023-2024</p>
-	</footer>
+	<Footer />
 </div>
 
 <style>
@@ -43,7 +25,7 @@
 	}
 
 	:global(body.dark) .app {
-	background-color: var(--dark-bg-primary);
+		background-color: var(--dark-bg-primary);
 	}
 
 	main {
@@ -55,57 +37,5 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	footer div {
-		display: flex;
-		column-gap: 10px;
-		align-items: center;
-	}
-
-	footer div a {
-		width: 50px;
-		height: 50px;
-		align-items: center;
-		justify-content: center;
-		display: flex;
-	}
-
-	.facebook-button {
-		background-color: blue;
-	}
-
-	.instagram-button {
-		background-color: blueviolet;
-	}
-
-	.twitter-button {
-		background-color: black;
-	}
-
-	.youtube-button {
-		background-color: red;
-	}
-
-	footer div a img {
-		width: 30px;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
