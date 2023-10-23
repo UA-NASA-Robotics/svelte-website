7c510fe4a42e71b85dc48907f1260121ceaf4403
Eli <elimbukoski@gmail.com>
2023-10-23 13:20:01 -0400

    fix flexbox columns

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 <h1>Who We Are</h1>
 <p>The NASA Robotic Mining Team is a student-led engineering design team that designs and builds an autonomous robot for lunar mining. The robot’s design purpose is to traverse and mine in a simulated lunar environment collecting materials 30 cm beneath the surface. Every May, team members spend a week at NASA’s Kennedy Space Center in Florida, where the robot competes against robots built by approximately 50 other teams from other universities all over the US. The team is also focused strongly on K-12 STEM outreach, especially to underrepresented students.</p>
 
-<div class="colunms">
+<div class="flex-columns">
 <div class="thirds">
 <h3>New Members</h3>
 <p>Are you a University of Akron student? Are you interested in joining our team?
