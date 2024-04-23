b94f2c90e86f91d84732831fb36ddafd8e75effa
Eli <elimbukoski@gmail.com>
2024-04-23 17:59:17 -0400

    add robots page/component

--- src/routes/our-history/+page.svelte
+++ src/routes/our-history/+page.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
 	import './style.css';
 
-	let promise = handleFetchRobots();
-
-	async function handleFetchRobots() {
-		let response = await fetch('/src/lib/data/robots.json', { method: 'GET' });
-		return await response.json();
-	}
-
+	import Robots from '../../components/Robots.svelte';
 </script>
 
 <svelte:head>
@@ -29,44 +23,6 @@
 		The Akron NASA Robotics team has competed every year NASA has hosted the competition. Here are a
 		the robots of the Akron RMC team career!
 	</p>
-	<div class="robotContent">
-		{#await promise}
-			<p>Loading...</p>
-		{:then robots}
-			{#if robots.length <= 0}
-				<div class="robotCard"><p>There are no robot biographies at this time.</p></div>
-			{:else}
-				{#each robots as robot, index}
-					<div class={index % 2 == 0 ? 'robotCard' : 'robotCard robotCardReverse'}>
-						{#if robot.image && robot.image.length > 0}
-							<div class="robotImageDiv">
-								<img src={robot.image} alt="robot Thumbnail" class="robotImg" />
-							</div>
-						{:else}
-							<!-- No image -->
-						{/if}
 
-						<div class="robotText">
-							<div class="flex-columns">
-								<div class="float-left">
-									<h2>{robot.name}</h2>
-								</div>
-								<div class="float-right">
-									<h3>{robot.year}</h3>
-								</div>
-							</div>
-							<hr class="bar" />
-							<h3>{robot.acronym}</h3>
-							<p>{robot.description}</p>
-						</div>
-					</div>
-				{/each}
-			{/if}
-		{:catch error}
-			<div class="robotCard">
-				<p>There was an error loading the robots. Please try again later.</p>
-				<br /><button on:click={window.location.reload}>Refresh</button>
-			</div>
-		{/await}
-	</div>
+	<Robots input="a" />
 </div>
