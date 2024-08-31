573a6dcb70fcec6619cbe67e23043cf75f7e5c4c
Eli <elimbukoski@gmail.com>
2024-08-30 23:45:25 -0400

    update sponsor prototype

--- src/routes/sponsors/Sponsors.svelte
+++ src/routes/sponsors/Sponsors.svelte
@@ -1,7 +1,5 @@
 <script>
-	import logo from '$lib/images/Logo.png';
-
-	import './Sponsors.css';
+	import './SponsorCard.svelte';
 
 	import ua from '$lib/images/sponsors/UniversityofAkron.jpg';
 	import altium from '$lib/images/sponsors/altium.jpg';
@@ -11,87 +9,253 @@
 	import schaeffler from '$lib/images/sponsors/schaeffler.jpg';
 	import pozyx from '$lib/images/sponsors/pozyx.jpg';
 	import andersonPower from '$lib/images/sponsors/anderson-power.png';
+	import SponsorCard from './SponsorCard.svelte';
 
-	const sponsorsList = [
-		{
-			logo: ua,
-			name: 'University of Akron',
-			link: 'http://www.uakron.edu/',
-			majorSponsor: true
-		},
-		{
-			logo: altium,
-			name: 'Altium',
-			link: 'http://www.altium.com/',
-			majorSponsor: false
-		},
-		{
-			logo: pepperlFuchs,
-			name: 'Pepperl-Fuchs',
-			link: 'http://www.pepperl-fuchs.us/',
-			majorSponsor: false
-		},
-		{
-			logo: osgc,
-			name: 'Ohio Space Grant Consortium',
-			link: 'http://www.osgc.org/',
-			majorSponsor: true
-		},
-		{
-			logo: wardjet,
-			name: 'WardJet',
-			link: 'http://www.wardjet.com/',
-			majorSponsor: false
-		},
-		{
-			logo: schaeffler,
-			name: 'Schaeffler',
-			link: 'http://www.schaeffler.us/',
-			majorSponsor: false
-		},
-		{
-			logo: pozyx,
-			name: 'Pozyx',
-			link: 'http://www.pozyx.io/',
-			majorSponsor: false
-		},
-		{
-			logo: andersonPower,
-			name: 'Anderson Power Products',
-			link: 'http://www.andersonpower.com/',
-			majorSponsor: true
-		}
-	];
+	const sponsorsList = {
+		tierFourSponsors: [
+			{
+				logo: ua,
+				name: 'University of Akron',
+				link: 'http://www.uakron.edu/',
+				description:
+					'The University of Akron provides funding and resources to the Robotics Mining Team. It is the home of the team!'
+			},
+			{
+				logo: ua,
+				name: 'University of Akron',
+				link: 'http://www.uakron.edu/',
+				description:
+					'The University of Akron provides funding and resources to the Robotics Mining Team. It is the home of the team!'
+			},
+			{
+				logo: ua,
+				name: 'University of Akron',
+				link: 'http://www.uakron.edu/',
+				description:
+					'The University of Akron provides funding and resources to the Robotics Mining Team. It is the home of the team!'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/',
+				description:
+					'Anderson Power Products is a leader in developing high quality, low cost, power interconnect solutions for industries that require high levels of reliability and performance.'
+			},
+			{
+				logo: osgc,
+				name: 'Ohio Space Grant Consortium',
+				link: 'http://www.osgc.org/',
+				description:
+					'The Ohio Space Grant Consortium is a part of the National Space Grant College and Fellowship Program. It is dedicated to funding students and faculty in STEM fields.'
+			},
+			{
+				logo: wardjet,
+				name: 'WardJet',
+				link: 'http://www.wardjet.com/',
+				description:
+					'WardJet is an industry leader in waterjet cutting technology. They provide the team with resources and support to help us succeed.'
+			}
+		],
+		tierThreeSponsors: [
+			{
+				logo: osgc,
+				name: 'Ohio Space Grant Consortium',
+				link: 'http://www.osgc.org/'
+			},
+			{
+				logo: osgc,
+				name: 'Ohio Space Grant Consortium',
+				link: 'http://www.osgc.org/'
+			},
+			{
+				logo: osgc,
+				name: 'Ohio Space Grant Consortium',
+				link: 'http://www.osgc.org/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			},
+			{
+				logo: wardjet,
+				name: 'WardJet',
+				link: 'http://www.wardjet.com/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			},
+			{
+				logo: pepperlFuchs,
+				name: 'Pepperl-Fuchs',
+				link: 'http://www.pepperl-fuchs.us/'
+			}
+		],
+		tierTwoSponsors: [
+			{
+				logo: altium,
+				name: 'Altium',
+				link: 'http://www.altium.com/'
+			},
+			{
+				logo: altium,
+				name: 'Altium',
+				link: 'http://www.altium.com/'
+			},
+			{
+				logo: altium,
+				name: 'Altium',
+				link: 'http://www.altium.com/'
+			},
+			{
+				logo: altium,
+				name: 'Altium',
+				link: 'http://www.altium.com/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			},
+			{
+				logo: osgc,
+				name: 'Ohio Space Grant Consortium',
+				link: 'http://www.osgc.org/'
+			},
+			{
+				logo: wardjet,
+				name: 'WardJet',
+				link: 'http://www.wardjet.com/'
+			},
+			{
+				logo: pepperlFuchs,
+				name: 'Pepperl-Fuchs',
+				link: 'http://www.pepperl-fuchs.us/'
+			}
+		],
+		tierOneSponsors: [
+			{
+				logo: pepperlFuchs,
+				name: 'Pepperl-Fuchs',
+				link: 'http://www.pepperl-fuchs.us/'
+			},
+			{
+				logo: wardjet,
+				name: 'WardJet',
+				link: 'http://www.wardjet.com/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			},
+			{
+				logo: osgc,
+				name: 'Ohio Space Grant Consortium',
+				link: 'http://www.osgc.org/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			},
+			{
+				logo: pepperlFuchs,
+				name: 'Pepperl-Fuchs',
+				link: 'http://www.pepperl-fuchs.us/'
+			},
+			{
+				logo: wardjet,
+				name: 'WardJet',
+				link: 'http://www.wardjet.com/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			},
+			{
+				logo: osgc,
+				name: 'Ohio Space Grant Consortium',
+				link: 'http://www.osgc.org/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			},
+			{
+				logo: pepperlFuchs,
+				name: 'Pepperl-Fuchs',
+				link: 'http://www.pepperl-fuchs.us/'
+			},
+			{
+				logo: wardjet,
+				name: 'WardJet',
+				link: 'http://www.wardjet.com/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			},
+			{
+				logo: osgc,
+				name: 'Ohio Space Grant Consortium',
+				link: 'http://www.osgc.org/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			},
+			{
+				logo: pepperlFuchs,
+				name: 'Pepperl-Fuchs',
+				link: 'http://www.pepperl-fuchs.us/'
+			},
+			{
+				logo: wardjet,
+				name: 'WardJet',
+				link: 'http://www.wardjet.com/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			},
+			{
+				logo: osgc,
+				name: 'Ohio Space Grant Consortium',
+				link: 'http://www.osgc.org/'
+			},
+			{
+				logo: andersonPower,
+				name: 'Anderson Power Products',
+				link: 'http://www.andersonpower.com/'
+			}
+		]
+	};
 </script>
 
-<div class="sponsorContent">
-	{#if sponsorsList.length <= 0}
-		<div class="sponsorCard"><p>There are no sponsors at this time.</p></div>
-	{:else}
-		{#each sponsorsList as sponsor, index}
-			<div
-				class={sponsor.majorSponsor
-					? 'sponsorCard majorSponsor'
-					: index % 2 == 0
-					? 'sponsorCard'
-					: 'sponsorCard sponsorCardReverse'}
-			>
-				{#if sponsor.logo != undefined && sponsor.logo != ''}
-					<div class={!sponsor.majorSponsor ? 'sponsorImageDiv' : 'sponsorImageDivMajor'}>
-						<img src={sponsor.logo} alt="sponsor Thumbnail" class="sponsorImg" />
-					</div>
-				{:else}
-					<div class={!sponsor.majorSponsor ? 'sponsorImageDiv' : 'sponsorImageDivMajor'}>
-						<img src={logo} alt="sponsor placeholder" class="sponsorImg" />
-					</div>
-				{/if}
-
-				<div class={!sponsor.majorSponsor ? 'sponsorText' : 'sponsorTextMajor'}>
-					<h2>{sponsor.name}</h2>
-					<hr class="bar" />
-					<p><a href={sponsor.link}>Visit {sponsor.name}.</a></p>
-				</div>
-			</div>
-		{/each}
-	{/if}
+<div class="sponsorContentContainer">
+	<SponsorCard sponsorsList={sponsorsList.tierFourSponsors} tier={4} />
+	<div style="height: 5vh;"></div>
+	<SponsorCard sponsorsList={sponsorsList.tierThreeSponsors} tier={3} />
+	<div style="height: 5vh;"></div>
+	<SponsorCard sponsorsList={sponsorsList.tierTwoSponsors} tier={2} />
+	<div style="height: 5vh;"></div>
+	<SponsorCard sponsorsList={sponsorsList.tierOneSponsors} tier={1} />
 </div>
